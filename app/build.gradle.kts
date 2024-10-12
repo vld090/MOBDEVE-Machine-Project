e36dcb87d69876011c67186eb0f3272e7da6d881
fatima_dy <136289175+pateemaa@users.noreply.github.com>
2024-10-12 23:28:50 +0800

    bottom navigation created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

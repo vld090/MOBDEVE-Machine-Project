7a28a10af14b11160f3cd1f06f0d55f6da92ec45
VL Dionela <valiantlance.dionela@gmail.com>
2024-11-09 20:08:03 +0800

    Initialized App

---
+++ app/src/main/java/com/mobdeveS19/group6/homevaultapp/Home.kt
@@ -0,0 +1,15 @@
+package com.mobdeveS19.group6.homevaultapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Home : Fragment(R.layout.fragment_home) {
+
+}

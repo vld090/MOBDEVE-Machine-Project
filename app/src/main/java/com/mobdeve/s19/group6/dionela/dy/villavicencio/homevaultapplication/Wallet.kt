26ca43843613f5a04564e1c10e85fc044dd8d7b0
VL Dionela <valiantlance.dionela@gmail.com>
2024-11-27 16:19:35 +0800

    Revert "Added database for Wallet"
    This reverts commit d49099e060be42fe1a0913b79fe839997dca5856.

--- app/src/main/java/com/mobdeve/s19/group6/dionela/dy/villavicencio/homevaultapplication/Wallet.kt
+++ app/src/main/java/com/mobdeve/s19/group6/dionela/dy/villavicencio/homevaultapplication/Wallet.kt
@@ -15,24 +15,26 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class Wallet : Fragment() {
     private lateinit var rvWallet: RecyclerView
     private lateinit var walletAdapter: WalletAdapter
-    private lateinit var walletDatabaseHelper: WalletDatabaseHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wallet, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        walletDatabaseHelper = WalletDatabaseHelper(requireContext())
-
         rvWallet = view.findViewById(R.id.rvWalletList)
         rvWallet.layoutManager = LinearLayoutManager(context)
 
-        val walletList = walletDatabaseHelper.getAllWallets() // Fetch wallet items from database
+        val walletList = listOf(
+            WalletItem(R.drawable.receipt1,"Receipt for Smart TV", "Smart TV","Samsung", "August 6, 2024", "August 6, 2023"),
+            WalletItem(R.drawable.receipt2, "PS5 Receipt", "PS5","Sony", "August 6, 2024", "August 6, 2023"),
+        )
+
         walletAdapter = WalletAdapter(walletList)
         rvWallet.adapter = walletAdapter
 
@@ -43,9 +45,10 @@ class Wallet : Fragment() {
     }
 
     private fun navigateToUploadForm() {
+        // Using FragmentManager to replace the current fragment with UploadReceiptFragment
         requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.flMainPage, UploadReceiptFragment())
-            .addToBackStack(null)
+            .replace(R.id.flMainPage, UploadReceiptFragment()) // Ensure you have a FrameLayout with id `flMainPage` in your activity layout to act as the container
+            .addToBackStack(null) // Allows users to return to the previous screen by pressing the back button
             .commit()
     }
 }
--- app/src/main/java/com/mobdeve/s19/group6/dionela/dy/villavicencio/homevaultapplication/WalletItem.kt
+++ app/src/main/java/com/mobdeve/s19/group6/dionela/dy/villavicencio/homevaultapplication/WalletItem.kt
@@ -6,6 +6,7 @@ data class WalletItem (
     val imageId: Int,
     val name: String,
     val assocItemName: String,
+    val assocItemBrand: String,
     val expiryDate: String,
     val createdDate: String
 )
